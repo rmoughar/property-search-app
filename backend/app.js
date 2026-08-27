7df6ca50d7e0acb066da9a9ef1191f2110db736b
rmoughar <rmoughar@umich.edu>
2026-08-27 12:49:42 -0400

    fix(deployment): adjust cors for public frontend env

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,8 @@ const app = express();
 const allowedOrigins = [
     'http://127.0.0.1:5173',
     'http://localhost:4005',
-    process.env.FRONTEND_URL
+    process.env.FRONTEND_URL,
+    process.env.PUBLIC_FRONTEND_URL
 ].filter(Boolean);
 
 app.use(cors({origin: allowedOrigins}));
