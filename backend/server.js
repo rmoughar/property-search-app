84eccd1135c2f3e88d20b547f9cb0cc193c21b3a
rmoughar <rmoughar@umich.edu>
2026-08-27 04:12:42 -0400

    fix(deployment): configure server for Render

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ import app from "./app.js"
 import 'dotenv/config';
 import { refreshPhotoValidations } from "./services/photoValidationRefresher.js";
 
-const port =  process.env.BACKEND_PORT || 4000;
+const port =  process.env.PORT || 4000;
 
-app.listen(port, (err) => {
+app.listen(port, "0.0.0.0", (err) => {
     if (err){
         console.error(err);
         return;
