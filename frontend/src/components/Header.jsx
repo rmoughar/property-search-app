494e4e386c8e95ec99fea674b14c693952218c3c
Ramy Mougharbel <rmoughar@umich.edu>
2026-08-27 01:05:16 -0400

    fix(header): correct header lettering

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,8 +8,10 @@ function Header(){
     return (
         <header className='app-header'>
             <div className='header-box'>
-                <Link className='title-link' to={'/'}><h1>Property Search |</h1></Link>
-                <Link className='favorites-link' to={'/favorites'}><h1>Favorites ({favorites.length}) |</h1></Link>
+                <Link className='title-link' to={'/'}><h1>Property Search</h1></Link>
+                <h1 className="divider">|</h1>
+                <Link className='favorites-link' to={'/favorites'}><h1>Favorites ({favorites.length})</h1></Link>
+                <h1 className="divider">|</h1>
                 <Link className='ai-search-link' to={'/ai-search'}><h1>AI Search</h1></Link>
             </div>
       </header>
